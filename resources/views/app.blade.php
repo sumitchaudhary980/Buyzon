691732deccd9f9ab0f289567c50e0a68aa2fa41f
sumitchaudhary980 <jaiswalsumit1010@gmail.com>
2025-02-09 19:20:44 +0545

    third commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="google-site-verification" content="ZAav9qFzSy6p2twmijKr807TsWR_MDydNfupJWCSqiA" />
-        <meta name="google-site-verification" content="ZAav9qFzSy6p2twmijKr807TsWR_MDydNfupJWCSqiA" />
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
